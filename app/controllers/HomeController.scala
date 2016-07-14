de7e8576304fa647542ab001239729561eb14d4a
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-07-14 10:09:23 +0800

    Add json response and query string support

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,23 +5,37 @@ import javax.inject._
 import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class HomeController @Inject() extends Controller {
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index = Action {
     Ok(views.html.index("Home Scala & Play."))
   }
 
-  def hello(name: String) = Action {
-    Ok(s"hello $name")
+  // /hello/zhenglai?title=Mr
+  def hello(name: String) = Action { request =>
+    val title = request.getQueryString("title").map(_+" ").getOrElse("")
+    Ok(s"hello $title$name")
+  }
+
+  def pictureFor(name: String) = Action {
+    Ok(s"Hello $name, this is your picture")
+  }
+
+  def hello2(first: String, last: String) = Action {
+    Ok(s"Hello $first => $last")
+  }
+
+  def json(key: String, value: String) = Action {
+    Results.Ok(play.api.libs.json.Json.obj(key -> value));
   }
 }
